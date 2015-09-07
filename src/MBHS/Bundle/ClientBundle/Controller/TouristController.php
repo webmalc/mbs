0b105c0a778f801769069cb087f81988a96f9d5f
Aleksandr Arofikin <sashaaro@gmail.com>
2015-09-07 13:30:40 +0300

    touristController

--- src/MBHS/Bundle/ClientBundle/Controller/TouristController.php
+++ src/MBHS/Bundle/ClientBundle/Controller/TouristController.php
@@ -1,15 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mb
- * Date: 07.09.15
- * Time: 13:25
- */
 
 namespace MBHS\Bundle\ClientBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class TouristController
+ * @Route("/tourist")
+ * @author Aleksandr Arofikin <sashaaro@gmail.com>
+ */
 class TouristController
 {
+    /**
+     * @Route("/add_black_list")
+     * @Method("GET")
+     * @param $request \Symfony\Component\HttpFoundation\Request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function addBlackListAction(Request $request)
+    {
+        
 
+        return new JsonResponse(['status' => true]);
+    }
 }
